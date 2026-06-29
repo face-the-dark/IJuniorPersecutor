5333d2ee545a6aa4b659758ec13a552156d3b419
Nikolai <ne360cosmos@gmail.com>
2026-06-30 01:19:16 +0300

    Настроена сцена

---
+++ src/IJuniorPersecutor/Assets/Game/Code/Bot.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Game.Code
+{
+    public class Bot : MonoBehaviour
+    {
+    
+    }
+}
---
+++ src/IJuniorPersecutor/Assets/Game/Code/Player.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Game.Code
+{
+    public class Player : MonoBehaviour
+    {
+    
+    }
+}
